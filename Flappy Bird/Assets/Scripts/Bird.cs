2a5abb74b4fc869552110268d06d6838647a67c1
Dima <magas.dm99@gmail.com>
2021-12-03 19:01:58 +0100

    Added Bird

---
+++ Flappy Bird/Assets/Scripts/Bird.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Bird : MonoBehaviour
+{
+    [SerializeField] private float _jumpForce;
+    private Rigidbody2D _rigidbody2D;
+
+
+    private void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        _rigidbody2D.velocity = Vector2.up * _jumpForce;
+    }
+}
--- Flappy Bird/Assets/Scripts/MovingBackground.cs
+++ Flappy Bird/Assets/Scripts/MovingBackground.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class MovingBackground : MonoBehaviour
 {
+    //[SerializeField] private float _speed = 2;
+    //private Transform _backTransform;
+    //private float _backgroundSize;
+    //private float _backgroundPosition;
+
+    //private void Start()
+    //{
+    //    _backTransform = GetComponent<Transform>();
+    //    _backgroundSize = GetComponent<SpriteRenderer>().bounds.size.x;
+    //    Debug.Log(_backgroundSize);
+    //}
+
+    //private void Update()
+    //{
+    //    Move();
+    //}
+
+    //private void Move()
+    //{
+    //    _backgroundPosition -= _speed * Time.deltaTime;
+    //    _backgroundPosition = Mathf.Repeat(_backgroundPosition, _backgroundSize);
+    //    _backTransform.position = new Vector3(_backgroundPosition, 0, 0);
+    //    Debug.Log(_backgroundPosition);
+    //}
+
     [SerializeField] private float _speed = 2;
-    private Transform _backTransform;
-    private float _backSize;
-    private float _backPos;
+    private Transform _backgroundTransform;
+    private float _backgroundSize;
+    private float _backgroundPosition;
 
     private void Start()
     {
-        _backTransform = GetComponent<Transform>();
-        _backSize = GetComponent<SpriteRenderer>().bounds.size.x;
+        _backgroundTransform = GetComponent<Transform>();
+        _backgroundSize = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
     private void Update()
@@ -22,8 +47,8 @@ public class MovingBackground : MonoBehaviour
 
     private void Move()
     {
-        _backPos -= _speed * Time.deltaTime;
-        _backPos = Mathf.Repeat(_backPos, _backSize);
-        _backTransform.position = new Vector3(_backPos, 0, 0);
+        _backgroundPosition -= _speed * Time.deltaTime;
+        _backgroundPosition = Mathf.Repeat(_backgroundPosition, _backgroundSize);
+        _backgroundTransform.position = new Vector3(_backgroundPosition, 0, 0);
     }
 }
